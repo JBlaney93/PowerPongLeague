c4f47b32bc4af829b998e1e1e32dcaba416bc5e5
Séamus <ryans40@tcd.ie>
2023-01-20 21:28:33 +0000

    got a get all working

--- server/server.js
+++ server/server.js
@@ -1,18 +1,32 @@
+// setup
+
 const express = require('express');
 const app = express();
+const port = 9000;
 app.use(express.json());
 
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 app.use(cors());
 
-MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology:true})
-    .then(client => {
-        const db = client.db('pingpong');
-    }
-)
+const createRouter = require('./helpers/create_router.js');
 
-app.listen(9000, function () {
-    console.log(`Listening on port ${this.address().port}`)
+// connect to database
+
+MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology:true})
+.then(client => {
+    const db = client.db('pingpong');
+    const playerCollection = db.collection('players');
+
+    const playerRouter = createRouter(playerCollection);
+    app.use('/api/players', playerRouter);
+
+})
+.catch(console.error);
+
+// listen
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`)
 })
 
