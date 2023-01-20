bd98de3725ca8fcb0686b44d188d56b52d9509c4
Séamus <ryans40@tcd.ie>
2023-01-20 18:27:31 +0000

    added game and player objects

---
+++ server/db/data.js
@@ -0,0 +1,32 @@
+const players = [
+    {name: "Fedau"},
+    {name: "Lee"},
+    {name: "Kyle"},
+    {name: "Kelsie"},
+    {name: "James Boag"},
+    {name: "Josh Mackie"},
+    {name: "Séamus"},
+    {name: "James Blaney"},
+    {name: "Jill"}
+];
+
+const games = [
+    {
+        datetime: new Date(),
+        winner_score: 11,
+        loser_score: 8,
+        winner_id: null,
+        winner_name: "Séamus",
+        loser_id: null,
+        loser_name: "Jill"
+    },
+    {
+        datetime: new Date(),
+        winner_score: 11,
+        loser_score: 8,
+        winner_id: null,
+        winner_name: "James Blaney",
+        loser_id: null,
+        loser_name: "Séamus"
+    }
+];
