5deb89ff4d56591f25ba97b474725dc09a21323b
James Blaney <jamesblaney93@gmail.com>
2023-01-20 14:46:50 +0000

    PlayerForm.js added and rendering hello message in MainContainer.js

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,10 +1,12 @@
 import GameForm from "../components/GameForm";
+import PlayerForm from "../components/PlayerForm";
 
 const MainContainer = () => {
     return(
         <div>
             <p>Hello from MainContainer.js</p>
             <GameForm />
+            <PlayerForm />
         </div>
     )
 }
