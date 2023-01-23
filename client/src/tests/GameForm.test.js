f537028ef4401197550e6e158c65b15f2bdcfac8
James Blaney <jamesblaney93@gmail.com>
2023-01-23 16:21:12 +0000

    games sorting from most recent in history and displaying date/time

---
+++ client/src/tests/GameForm.test.js
@@ -0,0 +1,17 @@
+import {render, fireEvent, waitFor} from '@testing-library/react';
+import GameForm from '../components/GameForm';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('GameForm ', () => {
+    let container;
+    beforeEach(() => {
+      container = render(<GameForm/>, {wrapper: BrowserRouter})
+    })
+
+    // it('should start counter at 0', () => {
+    //     const counter = container.getByTestId("counter-button")
+    //     expect(counter.textContent).toEqual('0');
+    // })
+
+
+})
