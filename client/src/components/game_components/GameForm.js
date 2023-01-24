e59aeb3131094d6596f3f68afb7d9ec9603fc990
Séamus <ryans40@tcd.ie>
2023-01-24 19:31:16 +0000

    got the names and scores displaying in the win screen

--- client/src/components/game_components/GameForm.js
+++ client/src/components/game_components/GameForm.js
@@ -7,103 +7,100 @@ import WinScreen from "./WinScreen";
 
 const GameForm = ({ addToGameHistory, players }) => {
 
-   // stages for displaying items
-   const [playersSelected, setPlayersSelected] = useState(false);
-   const [playersConfirmed, setPlayersConfirmed] = useState(false);
-   const [gameWon, setGameWon] = useState(false);
+    // stages for displaying items
+    const [playersSelected, setPlayersSelected] = useState(false);
+    const [playersConfirmed, setPlayersConfirmed] = useState(false);
+    const [gameWon, setGameWon] = useState(false);
+    const [endGame, setEndGame] = useState({})
+
+    const [gamePlayers, setGamePlayers] = useState({
+        player1: false,
+        player2: false,
+    })
 
 
-   const [endGame, setEndGame] = useState({})
-   const [gamePlayers, setGamePlayers] = useState({
-       player1: false,
-       player2: false,
-   })
+    const handlePlayerSelect = (event) => {
+        const player = event.target.name
+        const temp = { ...gamePlayers }
 
 
-   const handlePlayerSelect = (event) => {
-       const player = event.target.name
-       const temp = { ...gamePlayers }
+        PlayerService.findPlayer(event.target.value)
+            .then(obj => temp[player] = obj)
+            .then(() => setGamePlayers(temp))
+    }
 
 
-       PlayerService.findPlayer(event.target.value)
-           .then(obj => temp[player] = obj)
-           .then(() => setGamePlayers(temp))
-   }
+    const handleWin = (player, w_score, l_score) => {
+        if (player === "player1") {
+            const endGame = {
+                datetime: new Date(),
+                winner_id: gamePlayers.player1._id,
+                loser_id: gamePlayers.player2._id,
+                w_score: w_score,
+                l_score: l_score
+            }
+            setEndGame(endGame);
+            setGameWon(true);
+        } else if (player === 'player2') {
+            const endGame = {
+                datetime: new Date(),
+                winner_id: gamePlayers.player2._id,
+                loser_id: gamePlayers.player1._id,
+                w_score: w_score,
+                l_score: l_score
+            }
+            setEndGame(endGame);
+            setGameWon(true);
+        }
+    }
 
 
-   const handleWin = (player, w_score, l_score) => {
-       if (player === "player1") {
-           const endGame = {
-               datetime: new Date(),
-               winner_id: gamePlayers.player1._id,
-               loser_id: gamePlayers.player2._id,
-               w_score: w_score,
-               l_score: l_score
-           }
-           setEndGame(endGame);
-           setGameWon(true);
-       } else if (player === 'player2') {
-           const endGame = {
-               datetime: new Date(),
-               winner_id: gamePlayers.player2._id,
-               loser_id: gamePlayers.player1._id,
-               w_score: w_score,
-               l_score: l_score
-           }
-           setEndGame(endGame);
-           setGameWon(true);
+    useEffect(() => {
+        if (gamePlayers.player1 && gamePlayers.player2) {
+            setPlayersSelected(true)
+        }
+    }, [gamePlayers])
 
 
-       }
-   }
+    const handleSaveGame = () => {
+        if (!gameWon) {
+            return;
+        }
+        addToGameHistory(endGame);
+    }
 
 
-   useEffect(() => {
-       if (gamePlayers.player1 && gamePlayers.player2) {
-           setPlayersSelected(true)
-       }
-   }, [gamePlayers])
+    const handleConfirm = () => {
+        if (!playersSelected) {
+            return;
+        }
+        setPlayersConfirmed(true);
+    }
 
 
-   const handleSaveGame = () => {
-       if (!gameWon) {
-           return;
-       }
-       addToGameHistory(endGame);
-   }
+    return (
+        <div>
+            {!playersConfirmed ?
+                <div className="player-select">
+                    <fieldset className="player-select-form">
+                        <legend className="select-to-proceed">SELECT TWO PLAYERS TO PROCEED:</legend>
+                        <PlayerSelect players={players} handlePlayerSelect={handlePlayerSelect} gamePlayers={gamePlayers} />
+                        <button className={playersSelected ? "confirm-button" : "inactive-confirm-button"} onClick={handleConfirm}>CONFIRM</button>
+                    </fieldset>
+                </div>
+                : null}
 
 
-   const handleConfirm = () => {
-       if (!playersSelected) {
-           return;
-       }
-       setPlayersConfirmed(true);
-   }
+            {playersConfirmed && !gameWon ?
+                <div className="counters">
+                    <Counters handleWin={handleWin} player1={gamePlayers.player1.name} player2={gamePlayers.player2.name} />
+                </div>
+                : null}
 
 
-   return (
-       <div>
-           {!playersConfirmed ?
-               <div className="player-select">
-                   <fieldset className="player-select-form">
-                       <legend className="select-to-proceed">SELECT TWO PLAYERS TO PROCEED:</legend>
-                       <PlayerSelect players={players} handlePlayerSelect={handlePlayerSelect} gamePlayers={gamePlayers} />
-                       <button className={playersSelected?"confirm-button":"inactive-confirm-button"} onClick={handleConfirm}>CONFIRM</button>
-                   </fieldset>
-               </div>
-               : null}
-
-
-           {playersConfirmed && !gameWon ?
-               <div className="counters">
-                   <Counters handleWin={handleWin} player1={gamePlayers.player1.name} player2={gamePlayers.player2.name} />
-               </div>
-               : null}
-
-
-           {gameWon ?
-               <WinScreen handleSaveGame={handleSaveGame} />
-               : null}
+            {gameWon ?
+                <WinScreen handleSaveGame={handleSaveGame} endGame={endGame} />
+                : null}
 
 
 
@@ -118,11 +115,11 @@ const GameForm = ({ addToGameHistory, players }) => {
                     RECENT GAMES</Link>
             </div>
 
-           {gameWon ?
-               <Link className="back-to-menu" to="/game-form">PLAY AGAIN</Link>
-               : null}
-       </div>
-   )
+            {gameWon ?
+                <Link className="back-to-menu" to="/game-form">PLAY AGAIN</Link>
+                : null}
+        </div>
+    )
 }
 
 
