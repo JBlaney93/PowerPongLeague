d124deabc991050ac76429ee7776180699a27733
jilleatkins <jilleatkins@gmail.com>
2023-01-24 12:04:02 +0000

    Fixed player select form

--- client/src/components/game_components/GameHistory.js
+++ client/src/components/game_components/GameHistory.js
@@ -2,26 +2,30 @@ import { Link } from "react-router-dom";
 import GameItem from "../game_components/GameItem"
 import React from "react";
 
+
 const GameHistory = ({ gameHistory }) => {
 
-    // console.log(gameHistory);
-    
-    const gameList = gameHistory.slice(0).reverse().map((game, index) => {
-        return <GameItem
-                    game={game}
-                    key={index}
-                />
-    })
 
-    return(
-            <div>
-                <p>GAME HISTORY HERE</p>
-                <ul>
-                    {gameList}
-                </ul>
-                <Link to="/">Home</Link>
-            </div>
-    )
+   // console.log(gameHistory);
+  
+   const gameList = gameHistory.slice(0).reverse().map((game, index) => {
+       return <GameItem
+                   game={game}
+                   key={index}
+               />
+   })
+
+
+   return(
+           <div>
+               <p>GAME HISTORY HERE</p>
+               <ul>
+                   {gameList}
+               </ul>
+               <Link className ="back-to-menu" to="/">BACK TO MAIN MENU</Link>
+           </div>
+   )
 }
+
 
 export default GameHistory;
